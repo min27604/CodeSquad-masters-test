e65f57c63eca9a634d05b0dcc6430384aee00f83
Sally Oh <min27604@hotmail.com>
2020-12-09 00:01:21 +0900

    Update moving methods(left, right, up and down)

--- src/Step2.java
+++ src/Step2.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Step2 {
-    public void printArr(String[][] arr) {
+    String[][] cube = {{"R", "R", "W"}, {"G", "C", "W"}, {"G", "B", "B"}};
+
+    public void printCube(String[][] arr) {
         for (String[] strings : arr) {
             for (String string : strings) {
                 System.out.print(string + " ");
@@ -12,8 +14,7 @@ public class Step2 {
     }
 
     public void init(Scanner in) {
-        String[][] cube = {{"R", "R", "W"}, {"G", "C", "W"}, {"G", "B", "B"}};
-        printArr(cube);
+        printCube(cube);
         takeInputs(in);
     }
 
@@ -25,6 +26,38 @@ public class Step2 {
         } else {
             // TODO: MAKE METHOD THAT MOVES THE CUBE ACCORDING TO THE DIRECTION(INPUT)
         }
+    }
+
+    public void moveLeft(int index) {
+        String temp = cube[index][0];
+        for (int i = 0; i < cube[index].length - 1; i++) {
+            cube[index][i] = cube[index][i + 1];
+        }
+        cube[index][cube[index].length] = temp;
+    }
+
+    public void moveRight(int index) {
+        String temp = cube[index][cube[index].length - 1];
+        for (int i = cube[index].length - 1; i > 0; i--) {
+            cube[index][i] = cube[index][i - 1];
+        }
+        cube[index][0] = temp;
+    }
+
+    public void moveUp(int index) {
+        String temp = cube[0][index];
+        for (int i = 0; i < cube.length - 1; i++) {
+            cube[i][index] = cube[i + 1][index];
+        }
+        cube[cube.length - 1][index] = temp;
+    }
+
+    public void moveDown(int index) {
+        String temp = cube[cube.length - 1][index];
+        for (int i = cube.length - 1; i > 0; i--) {
+            cube[i][index] = cube[i - 1][index];
+        }
+        cube[0][index] = temp;
     }
 
     public static void main(String[] args) {
