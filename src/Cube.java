bbcdb7c652817847343770c33bfc766a075a07aa
Sally Oh <min27604@hotmail.com>
2020-12-11 01:17:15 +0900

    Get directions according to the input and specify if the direction is reverse or double turn

--- src/Cube.java
+++ src/Cube.java
@@ -16,18 +16,56 @@ public class Cube {
 
     private void start() {
         printer.printCube(cube);
-        while(true) {
+        while (true) {
             takeInputs();
             if (input.equals("Q")) {
                 System.out.println("조작갯수: " + counter);
                 System.out.println("이용해주셔서 감사합니다. 뚜뚜뚜.");
                 break;
             }
+            getDirection();
         }
     }
 
     private void takeInputs() {
         System.out.print("CUBE> ");
         input = sc.nextLine();
+    }
+
+    private void getDirection() {
+        for (int i = 0; i < input.length(); i++) {
+            String direction;
+
+            if (input.charAt(i) == '\'') {
+                continue;
+            }
+
+            if (input.charAt(i) == '2') {
+                direction = specifyDoubleTurn(input, i);
+            } else {
+                direction = specifyReverse(input, i);
+            }
+
+            System.out.println(direction + "\n");
+        }
+    }
+
+    private String specifyReverse(String input, int i) {
+        String tempDirection = "" + input.charAt(i);
+        if ((i != input.length() - 1) && (input.charAt(i + 1) == '\'')) {
+            tempDirection += input.charAt(i + 1);
+        }
+        return tempDirection;
+    }
+
+    private String specifyDoubleTurn(String input, int i) {
+        String tempDirection = "";
+        if (input.charAt(i - 1) == '\'') {
+            tempDirection += input.charAt(i - 2);
+            tempDirection += input.charAt(i - 1);
+        } else {
+            tempDirection += input.charAt(i - 1);
+        }
+        return tempDirection;
     }
 }
