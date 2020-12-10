cef397e979c4780a7305b5ac818b0423d8d8113a
Sally Oh <min27604@hotmail.com>
2020-12-10 23:19:29 +0900

    Create constructor of cube class and basic method construction

--- src/Cube.java
+++ src/Cube.java
@@ -1,5 +1,30 @@
 public class Cube {
+    String[][][] cube = {{{"W", "W", "W"}, {"W", "W", "W"}, {"W", "W", "W"}},
+            {{"O", "O", "O"}, {"O", "O", "O"}, {"O", "O", "O"}}, {{"G", "G", "G"}, {"G", "G", "G"}, {"G", "G", "G"}},
+            {{"R", "R", "R"}, {"R", "R", "R"}, {"R", "R", "R"}}, {{"B", "B", "B"}, {"B", "B", "B"}, {"B", "B", "B"}},
+            {{"Y", "Y", "Y"}, {"Y", "Y", "Y"}, {"Y", "Y", "Y"}}};
+
     public Cube() {
+        init();
+    }
+
+    private void init() {
+        printCube();
+    }
+
+    private void printCube() {
+
+    }
+
+    private void printUp() {
+
+    }
+
+    private void printMiddle() {
+
+    }
+
+    private void printDown() {
 
     }
 }
